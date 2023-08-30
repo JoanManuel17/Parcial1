1009a229f82be2d75932167eaac263006602f696
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-08-29 22:59:35 -0500

    Initial commit

---
+++ parcial01/src/assets/componente1.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const Componente = () => {
+  const [segundos, setSegundos] = useState(0);
+  const [horas, setHoras] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+
+  const convertir = () => {
+    setHoras(Math.floor(segundos / 3600));
+    setMinutos(Math.floor((segundos % 3600) / 60));
+    setSegundos(Math.floor((segundos % 3600) % 60));
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Componente convertir segundos</h1>
+        <input
+          type="number"
+          value={segundos}
+          onChange={(evt) => setSegundos(evt.target.value)}
+        ></input>
+
+        <button onClick={() => convertir()}> Convertir </button>
+        <p>
+          {horas} horas, {minutos} minutos y {segundos} segundos
+        </p>
+      </div>
+    </>
+  );
+};
---
+++ parcial01/src/assets/componente3.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const Componente3 =  ()  => {
+    const [nombre, setNombre] = useState('')
+    const [hora, setHora] = useState(0)
+    const [mensaje, setMensaje] = useState('')
+    
+    const saludar = () => {
+        if(hora >= 0 && hora < 12){
+        setMensaje(`Buenos días, ${nombre}`)
+        }else if(hora >= 12 && hora < 18){
+        setMensaje(`Buenos tardes, ${nombre}`)
+        }else{
+        setMensaje(`Buenas noches, ${nombre}`)
+        }
+    }
+    
+    return (
+        <>
+        <div>
+            <h1>Componente saludar</h1>
+            <input type="text" value={nombre} 
+            onChange={(evt) => setNombre(evt.target.value)} ></input>
+
+            <input type="number" value={hora} 
+            onChange={(evt) => setHora(evt.target.value)} ></input>
+            <button onClick={ () => saludar()}> Saludar </button>
+            <p>
+            {mensaje}
+            </p>
+        </div>
+        </>
+    )
+    }
